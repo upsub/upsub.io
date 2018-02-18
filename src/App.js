3210be40e6af40614d50037faac2f59ba90096db
kvartborg <hello@kvartborg.net>
2018-02-18 20:21:21 +0100

    Disabled community

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 import Index from 'views/Index'
 import GettingStarted from 'views/GettingStarted'
 import Docs from 'views/Docs'
-import Community from 'views/Community'
+// import Community from 'views/Community'
 import NotFound from 'views/NotFound'
 
 global.h = h
@@ -15,7 +15,7 @@ const App = () => (
     <Index path='/' />
     <GettingStarted path='/getting-started/:section?' />
     <Docs path='/docs/:version?/:section?' />
-    <Community path='/community/:section?' />
+    {/* <Community path='/community/:section?' /> */}
     <NotFound default />
   </Router>
 )
